14756d7a71032d7f2ce3a66591146f3f5fa7845e
Jason Nguyen <jasonnguyen185@gmail.com>
2026-02-18 20:09:48 -0800

    Version 2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 #include "TextGraphics.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 
 int main () {
 
+    TextGraphics tg;
 
+    srand(time());
 
+    vector<int> drops(tg.width());
+    vector<int> speed(tg.width());
 
+    for (int i = 0; i < tg.width(); i++) {
+
+        drops[i] = rand() % tg.height();
+        speed[i] = 1 + rand() % 3;
+    }
+while (true) {
+    tg.clearScreen();
+    for (int j = 0; j < tg.width(); j++) {
+
+        int y = drops[x];
+
+        char headChar = 33 + rand() % 94;
+        tg.putCharAt(j, y, headchar, Color::White);
+        for (int tail = 1; tail <= 5; tail++) {
+            int drawY = y - tail;
+            if (drawY >= 0) {
+                char tailChar - 33 + rand() % 94;
+                tg.putCharAt(j, drawY, tailChar, Color::Green);
+            }
+        }
+        drops[j] += speed[j];
+        if (drops[j] >= tg.height()) {
+            drops [j] = 0;
+        }
+    }
+
+    tg.draw();
+    tg.sleepMs(50);
+}
     
-
 return 0;
-
 
 }
